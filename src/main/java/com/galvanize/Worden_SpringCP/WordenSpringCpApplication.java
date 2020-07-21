5d879467077e0a1968879cf1b050da5b0ffe374f
cdub020 <63630268+cdub020@users.noreply.github.com>
2020-07-21 07:31:36 -0700

    Initial Commit

---
+++ src/main/java/com/galvanize/Worden_SpringCP/WordenSpringCpApplication.java
@@ -0,0 +1,24 @@
+package com.galvanize.Worden_SpringCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WordenSpringCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordenSpringCpApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/galvanize/Worden_SpringCP/WordenSpringCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.Worden_SpringCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordenSpringCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
